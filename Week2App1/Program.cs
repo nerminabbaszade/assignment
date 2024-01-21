e26f5a36d513a8ad6d9f7d330209b22e63e7e94b
Nermin Abbaszade <narminma@code.edu.az>
2024-01-21 17:41:37 +0400

    uploading my assignment

---
+++ Week2App1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Week2App1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1, num2;
+            Console.WriteLine("Please enter first number:");
+            num1=float.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter second number:");
+            num2 = float.Parse(Console.ReadLine());
+            Console.WriteLine($"A + B: {num1 + num2}\n");
+            Console.WriteLine($"A - B: {num1 - num2}");
+            Console.WriteLine("A x B:"+Math.Round((num1 * num2),2));
+            
+            if (num2 != 0)
+            {
+                Console.WriteLine($"A : B: {Math.Round((num1 / num2),2)}");
+            }
+            else
+            {
+                Console.WriteLine("Cannot be divided by 0!");
+            }
+            
+        }
+    }
+}
